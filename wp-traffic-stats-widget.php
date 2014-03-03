1262541fcc17800f2a82533eb6814c404b3d5b60
helenthomaswp <helenthomaswp@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-03-03 14:11:37 +0000

    null
    git-svn-id: https://plugins.svn.wordpress.org/traffic-stats-widget/tags/1.0.1@868571 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-traffic-stats-widget.php
+++ wp-traffic-stats-widget.php
@@ -203,7 +203,7 @@ echo $ttl;?></p></strong>
   </ul>
 <?php 
 if ($options['wp_tsw_WidgetText_wlink'] == "on") { ?>
-<div align="center" style="display:none;"><small><a href="http://losangelesseoreview.wordpress.com/2014/01/12/bogus-negative-criticism-caught-for-bliss-drive-review/" target="_blank">Bliss Drive Review</a></small></div> 
+<div align="center" style="display:none;"><small><a href="https://sites.google.com/site/seolosangelesblissdrive/services/long-beach-seo-company" target="_blank">Long Beach SEO</a></small></div> 
 <?php } ?>
 
 <?php
